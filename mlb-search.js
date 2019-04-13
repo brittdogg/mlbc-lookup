df6bdd7bdc00722b347c80e784cb33574f4d4b10
Brittney Scaccia <bscaccia@trustwave.com>
2019-04-13 00:54:24 -0400

    Got marketplace search working

--- mlb-search.js
+++ mlb-search.js
@@ -1,66 +1,32 @@
-// get access to this page
-// scroll down to the search bar
-// enter player name in search bar
-
-// console.log(playerName);
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log("doing stuff...");
       // alert("HELLO " + request.player);
       // sendResponse({farewell: request.player});
       playerName = request.player;
-      doStuff();
+      lookup();
     });
 
-// window.onload = function() {
-    function doStuff() {
-        console.log("onload complete");
-        // let body = document.getElementsByTagName("body")[0];
-        //     console.log(body);
-
-        // let appRoot = document.getElementsByTagName("app-root")[0];
-        // console.log(appRoot);
+    function lookup() {
 
         function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
           }
           
-          async function demo() {
-            console.log('Taking a break...');
+          async function lookupSlowly() {
             await sleep(2000);
-            console.log('Two seconds later');
             let searchBar = document.querySelector("input");
-            console.log(searchBar);
-            searchBar.scrollIntoView();
             searchBar.value = playerName.trim();
-            var e = jQuery.Event("keydown");
-            e.which = 32; // # Some key code value
-            $("input").trigger(e);
-            await sleep(1000);
-            console.log($("fieldset > button")[0]);
-            $("fieldset > button")[0].click();
-            // document.querySelector("fieldset").querySelector("button").click();
-            // var f = jQuery.Event("keydown");
-            // f.which = 13; // # Some key code value
-            // $("input").trigger(f);
+            var inputEvent = new Event("input");
+            searchBar.dispatchEvent(inputEvent);
+            
+            var button = document.querySelector("fieldset > button");
+            var clickEvent = new Event("click");
+            button.dispatchEvent(clickEvent);
+            await sleep(500);
+            searchBar.scrollIntoView();
           }
           
-          demo();
+          lookupSlowly();
 
     }
-
-    var ev = new KeyboardEvent('keydown', {altKey:false,
-        bubbles: true,
-        cancelBubble: false, 
-        cancelable: true,
-        charCode: 0,
-        code: " ",
-        key: " ",
-        ctrlKey: false,
-        isComposing: false,
-        isTrusted: true,
-        key: " ",
-        shiftKey: false,
-        type: "keydown"
-    });
