aa0bc4c55d5dae64b69a77f31fb8b5435b3a2a02
Brittney Scaccia <bscaccia@trustwave.com>
2019-05-08 00:32:41 -0400

    Clicking popup adds SELL button to figures in inventory; SELL opens CryptoSlam & searches MLB Marketplace.

--- background.js
+++ background.js
@@ -5,7 +5,8 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {urlMatches: 'https://mlbc.app/asset/*'},
+            // pageUrl: {urlMatches: 'https://mlbc.app/figure/*'},
+            pageUrl: {urlMatches: 'https://mlbc.app/profile/figures'}
           })
           ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
--- content-script.js
+++ content-script.js
@@ -1,10 +1,17 @@
-console.log("hello MLBC");
-playerNameAndNumber = document.getElementsByClassName("show-subheading")[0].textContent;
-playerName = playerNameAndNumber.substr(0, playerNameAndNumber.indexOf("#"));
-console.log(playerName);
+// console.log("hello MLBC");
 
-// chrome.runtime.sendMessage({player: playerName}, function(response) {
-//     console.log("I got a response, here it is: ", response);
-// });
+// playerNameAndNumber = document.getElementsByClassName("show-subheading")[0].textContent;
 
-chrome.runtime.sendMessage({player: playerName});
+playerCards = document.querySelectorAll("section.es-assetcard");
+for (let player of playerCards) {
+    // console.log("player card: ", player);
+    let sellButton = document.createElement("BUTTON");
+    let buttonText = document.createTextNode("SELL");
+    sellButton.appendChild(buttonText);
+    sellButton.addEventListener("click", function() {
+        playerName = this.parentNode.parentNode.querySelector(".show-name").textContent;
+        name = playerName.substr(0, playerName.indexOf("#"));
+        chrome.runtime.sendMessage({player: name});
+    })
+    player.querySelector(".box-outercard").appendChild(sellButton);
+}
