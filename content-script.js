30dc0378b8c6ccc7a3a3448f3e58706c3597d16d
Brittney Scaccia <bscaccia@trustwave.com>
2019-06-02 21:27:22 -0400

    Prevent multiple sell buttons

--- content-script.js
+++ content-script.js
@@ -1,11 +1,12 @@
-// console.log("hello MLBC");
-
-// playerNameAndNumber = document.getElementsByClassName("show-subheading")[0].textContent;
-
+// add a SELL button to every player card on screen.
 playerCards = document.querySelectorAll("section.es-assetcard");
 for (let player of playerCards) {
-    // console.log("player card: ", player);
+    // prevent adding multiple sell buttons to the same card.
+    if (player.querySelector(".box-outercard").querySelector("#sellButton") != undefined) {
+        break;
+    }
     let sellButton = document.createElement("BUTTON");
+    sellButton.id = "sellButton";
     let buttonText = document.createTextNode("SELL");
     sellButton.appendChild(buttonText);
     sellButton.addEventListener("click", function() {
